51a3267d9cd2042b46a687c93a293500c17afb2d
surya1604surya <164052689+surya1604surya@users.noreply.github.com>
2025-06-12 15:34:17 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,12 +2,14 @@ import styles from "./ps.module.css";
 
 export default function about() {
   return (
+    
+    <div className={styles.homeBody}>
     <div className={styles.container}>
       <h1>About Accident Tracking</h1>
       <p>
         Our system is built to report and alert emergency contacts during vehicle accidents.
         We strive to improve safety by notifying registered contacts instantly.
       </p>
-    </div>
+    </div></div>
   );
 }
